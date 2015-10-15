029335b2de09c8b75dee33525a8490d9fbd52a18
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2015-10-15 18:22:35 +0200

    display version in SplashScreen

--- app/src/main/java/fr/emmanuel/loisance/androidstarter/global/SplashScreen.java
+++ app/src/main/java/fr/emmanuel/loisance/androidstarter/global/SplashScreen.java
@@ -2,8 +2,10 @@ package fr.emmanuel.loisance.androidstarter.global;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 import fr.emmanuel.loisance.androidstarter.R;
 import fr.emmanuel.loisance.androidstarter.activity.LoginActivity;
@@ -16,6 +18,15 @@ public class SplashScreen extends Activity {
         setContentView(R.layout.splash_screen);
 
         GlobalState gs = (GlobalState) getApplication();
+
+        // Version
+        TextView txtVersion = (TextView) findViewById(R.id.splash_txt_version);
+        try {
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            txtVersion.setText(getResources().getString(R.string.splash_txt_version_name, versionName));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
         Handler handler = new Handler();
         final Runnable r = new Runnable() {
